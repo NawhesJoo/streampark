f46fd280a6aa0170b1a0518759688f293ce73252
kim333333d <test102938t@gmail.com>
2023-05-22 09:02:26 +0900

    1
    1

--- src/main/java/com/project/controller/KSH/LoginController.java
+++ src/main/java/com/project/controller/KSH/LoginController.java
@@ -1,11 +1,22 @@
 package com.project.controller.KSH;
 
+import java.math.BigInteger;
+import java.util.Date;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.entity.Member;
+import com.project.repository.KSH.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 @Slf4j
@@ -13,12 +24,39 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/member")
 public class LoginController {
     
+    final MemberRepository mRepository;
+    final String format = "membercontroller => {}";
+    BigInteger token = new BigInteger("0");
+    Date date = new Date();
+    
+    BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+
+    // 메인 페이지
     @GetMapping(value = "/main.do")
     public String mainGET(){
         return "/KSH/main";
     }
-    @GetMapping(value = "login.do")
-    public String loginGET(){
-        return "/KSH/login";
+
+    // 가입페이지
+    @GetMapping(value = "/join.do")
+    public String joinGET(){
+        return "/KSH/join";
     }
+    @PostMapping(value = "/join.do")
+    public String joinPOST(@ModelAttribute Member obj) {
+        try {
+            obj.setPw(bcpe.encode(obj.getPw()));
+            obj.setRole("C");
+            obj.setToken(token);
+            obj.setRegdate(date);
+            log.info(format,obj.toString());
+
+            mRepository.save(obj);
+            return "redirect:/member/join.do";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/member/join.do";
+        }
+    }
+    
 }
---
+++ src/main/java/com/project/repository/KSH/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.project.repository.KSH;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.entity.Member;
+
+
+
+public interface MemberRepository extends JpaRepository<Member, String>{
+
+    long countById(String id);
+
+
+}
