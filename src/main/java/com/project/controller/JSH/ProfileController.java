06b704a0d1a370caf3e7e30b375e9d93570a5393
NawhesJoo <sefani08@naver.com>
2023-05-22 23:45:59 +0900

    20230522

--- src/main/java/com/project/controller/JSH/ProfileController.java
+++ src/main/java/com/project/controller/JSH/ProfileController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,56 +30,57 @@ import lombok.extern.slf4j.Slf4j;
 public class ProfileController {
 
     final String format = "ProfileContrller => {}";
-    final ProfileService profileService;
+    final ProfileService pService;
     final ProfileMapper pMapper;
     final ProfileRepository pRepository;
     final ProfileimgRepository piRepository;
     final HttpSession httpSession;
     BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
 
+
     // 프로필 선택창
-    @GetMapping(value = "/select.do")
-    public String selectGET(Model model, HttpSession session){
-        // 세션에서 멤버 id를 가져옴
-        // String id = (String) session.getAttribute("id");
-        String id ="id";
-        // 멤버에 연결된 프로필 목록
-        List<Profile> profiles = profileService.selectprofile(id);
-        model.addAttribute("profiles", profiles);
-        // log.info("profileimg", profiles.toString());
-        return "/JSH/StreamPark_profile";
+    @GetMapping(value = "/profilelist.do")
+    public String profilelistGET(Model model, HttpSession session){
+        try{
+            String id = "1";
+            List<Profile> list = pService.selectprofile(id);
+            model.addAttribute("list", list);
+            log.info("list => {}", list.toString());
+            return "/JSH/list";
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return "/JSH/list";
+        }
     }
+
+
 
     // 프로필 생성
     @GetMapping(value = "/create.do")
     public String createGET(Model model){
         model.addAttribute("profile", new Profile());
-        return "/JSH/StreamPark_profile";
+        return "/JSH/create";
     }
 
     @PostMapping(value = "/create.do")
     public String createPOST(@ModelAttribute("profile") Profile profile,
-        // @RequestParam("keyword") String keyword,
-        // @RequestParam("id") String id,
-        // @RequestParam("nickname") String nickname,
-        BindingResult result,
         HttpSession session) {
     // 세션에서 멤버 ID 가져오기
     // String memberId = (String) session.getAttribute("id");
-    String memberId = "1";
+    String memberId = "12";
 
     // 프로필 정보 설정
     Member member = new Member();
     member.setId(memberId);
-    // pMapper.createProfile(keyword, memberId, nickname);
     profile.setMember(member);
     log.info("profile =>", profile);
     // 프로필 저장
     pRepository.save(profile);
 
     // 프로필 생성 후 리다이렉트할 페이지 지정
-    return "redirect:/JSH/StreamPark_profile";
-    }
+    return "/JSH/hometest";
+    }   // 완료
 
     // 프로필 로그인
     @GetMapping(value = "/login.do")
@@ -92,16 +92,19 @@ public class ProfileController {
     public String loginPOST(@RequestParam("nickname") String nickname,
                             @RequestParam(value = "profilepw", required = false) String profilePw,
                             Model model, HttpSession session) {
+        session.getAttribute(nickname);
         if (profilePw == null || profilePw.isEmpty()) {
             // profilePw 값이 null이거나 빈 문자열인 경우에는 nickname만 입력된 경우이므로 로그인 성공
             session.setAttribute("nickname", nickname); // 세션에 nickname 저장
+            return "redirect:/profile/home.do";
         } else {
             // profilePw 값이 null이 아니고 비어있지 않은 경우에는 nickname과 profilepw가 모두 입력된 경우이므로 추가적인 로그인 처리를 수행할 수 있음
             // 비밀번호 검증 등의 로직을 수행할 수 있음
+
         }
         log.info("nickname => {}", nickname.toString());
         log.info("profilePw => {}", profilePw.toString());
-        return "redirect:/profile/home.do"; // 로그인 후 이동할 경로
+        return "redirect:/profile/home.do?" + nickname; // 로그인 후 이동할 경로
     }
 
     @GetMapping(value = "/home.do")
@@ -113,6 +116,8 @@ public class ProfileController {
             return "redirect:/profile/login.do"; // 로그인되지 않은 경우 로그인 폼으로 리다이렉트
         }
     }
+
+
 
     // 닉네임 변경
     @GetMapping(value = "/updatenickname.do")
@@ -130,7 +135,7 @@ public class ProfileController {
         Model model) {
     try {
         String currentnickname = "a1";
-        Profile profile1 = pRepository.findByNickname(currentnickname);
+        Profile profile1 = pService.findByNickname(currentnickname);
         profile1.setNickname(newNickname);
 
         if (bcpe.matches(profilepw, profile1.getProfilepw())) {
