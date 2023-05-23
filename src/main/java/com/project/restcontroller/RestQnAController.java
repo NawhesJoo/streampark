640e4dbf01d5150c18488dcd11e681e29fdca97e
minsoohyun <alstngus3@naver.com>
2023-05-24 01:25:48 +0900

    1
    1

--- src/main/java/com/project/restcontroller/RestQnAController.java
+++ src/main/java/com/project/restcontroller/RestQnAController.java
@@ -1,9 +1,10 @@
 package com.project.restcontroller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -19,8 +20,23 @@ public class RestQnAController {
 
     // Ajax 요청을 처리할 메소드
     @PostMapping("/delete.do")
-    public String checkPassword(@RequestParam String password) {       
-        log.info("format", password);
-        return "redirect:/qna/selectlist.do";
+    public boolean checkPassword(@RequestBody PasswordRequest request) {
+        String receivedPassword = request.getPassword(); // Ajax 요청에서 비밀번호 값을 받아옵니다.
+        String hashedPasswordFromDB = getPasswordFromDB();
+        log.info(format, hashedPasswordFromDB);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.matches(receivedPassword, hashedPasswordFromDB);
+    }
+
+    private String getPasswordFromDB() {
+        return null;
+    }
+}
+
+class PasswordRequest {
+    private String password;
+
+    public String getPassword() {
+        return null;
     }
 }
