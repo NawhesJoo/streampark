5752eaff9be9927718d8d905bbafec555f532c61
JANGMEN <joknek654@naver.com>
2023-05-24 18:44:06 +0900

    20230524 생일이니까 좀 빨리갈게
    20230524 생일이니까 좀 빨리갈게

--- src/main/java/com/project/jangrestcontroller/ReviewLikesPlusController.java
+++ src/main/java/com/project/jangrestcontroller/ReviewLikesPlusController.java
@@ -1,15 +1,15 @@
 package com.project.jangrestcontroller;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.entity.Review;
-import com.project.repository.JangRepositories.ReviewRepository;
+import com.project.mapper.ReviewMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +21,37 @@ import lombok.extern.slf4j.Slf4j;
 public class ReviewLikesPlusController {
 
     final String format = "reviewRestController => {}";
-    final ReviewRepository rRepository;
+    final ReviewMapper reviewMapper;
+    final BigInteger profileno = BigInteger.valueOf(6);
 
-    @PutMapping("/likesplus.json")
-    public String likespluscntPUT(@RequestParam(name = "review_no") BigInteger review_no, @RequestBody Review review) {
-        
-        return "/jang/review/selectlist";
+    @PutMapping(value="/reportplus.json")
+    public Map<String, Object> reportpluscntPUT(@RequestBody Map<String, Object> recvMap) {
+        log.info(format, recvMap.get("no"));
+        log.info(format, recvMap.get("pno"));
+        Map<String, Object> retMap = new HashMap<>();
+        // db처리
+        int ret = reviewMapper.reviewreport(recvMap);
+
+
+        long reportcnt = Integer.parseInt(String.valueOf(recvMap.get("no")));
+        Long select1 = reviewMapper.reviewreportselect(reportcnt);
+        log.info(format, select1);
+        // if (select1 >= 50) {
+        //     reviewMapper.reviewreportdelete(reportcnt);
+        // }
+        retMap.put("status", ret);
+        return retMap;
+    }
+
+    @PutMapping(value="/likesplus.json")
+    public Map<String, Object> likespluscntPUT(@RequestBody Map<String, Object> recvMap) {
+        log.info(format, recvMap.get("no"));
+        log.info(format, recvMap.get("pno"));
+        Map<String, Object> retMap = new HashMap<>();
+        // db처리
+        int ret = reviewMapper.reviewlikes(recvMap);
+        retMap.put("status", ret);
+        return retMap;
     }
     
 }
