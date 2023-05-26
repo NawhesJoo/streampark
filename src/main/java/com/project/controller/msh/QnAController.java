b72ddbb22f437340329813ec8c7b2e8ba0680a90
minsoohyun <alstngus3@naver.com>
2023-05-26 11:39:15 +0900

    1
    1

--- src/main/java/com/project/controller/msh/QnAController.java
+++ src/main/java/com/project/controller/msh/QnAController.java
@@ -1,7 +1,9 @@
 package com.project.controller.msh;
 
-import java.math.BigInteger;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.dto.Board;
 import com.project.mapper.QnaMapper;
@@ -17,7 +21,6 @@ import com.project.service.msh.QnaService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -27,6 +30,9 @@ public class QnAController {
     final String format = "QnA => {}";
     final QnaService qnaService;
     final QnaMapper qnaMapper;
+    final HttpSession httpSession;
+    final String message = "";
+	final String href = "";
 
     // 문의글 목록
     @GetMapping(value = "/selectlist.do")
@@ -49,8 +55,13 @@ public class QnAController {
 
     // 문의글 등록POST
     @PostMapping(value = "/insert.do")
-    public String insertPOST(@ModelAttribute Board board) {
+    public String insertPOST(@ModelAttribute Board board, HttpServletRequest request) {
         try {
+            // if (board.getProfileno() == null) {
+            //     request.setAttribute("msg", "로그인이 필요합니다.");
+            //     request.setAttribute("url", "/member/login");
+            //     return "alert";
+            // }
             BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
             board.setPassword(bcpe.encode(board.getPassword()));
             int ret = qnaService.insertBoard(board);
@@ -64,14 +75,27 @@ public class QnAController {
         }
     }
 
-    // 문의글 조회
+    // 내가 쓴 문의글 조회
+    @ResponseBody
     @GetMapping(value = "/selectone.do")
-    public String selectoneGET(Model model, @RequestParam(name = "no") Long no,@RequestParam(name = "profileno") Long profileno) {
-        
-        Board board = qnaService.selectoneBoard();
-        log.info("QnASelectone = {}", board.toString());
-        model.addAttribute("board", board);
-        return "/msh/selectone";
+    public String selectoneGET(Model model, @RequestParam(name = "no") Long no) {
+        try {
+            httpSession.setAttribute("profileno", 87L); //profileno매개변수에 87저장함.
+            Long profileno = (Long) httpSession.getAttribute("profileno");
+            // log.info("profileno = {}", profileno);
+    
+
+            Board obj = new Board();
+            obj.setNo(no);
+            obj.setProfileno(profileno);
+            log.info("QnASelectone = {}", obj.toString());
+            Board board = qnaService.selectoneBoard(obj);
+            model.addAttribute("board", board);
+            return "/msh/selectone";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/qna/selectlist.do";
+        }
     }
 
     // 문의글 수정
@@ -80,10 +104,10 @@ public class QnAController {
         if (no == 0) { // 번호 없으면 다시 목록으로 돌아감
             return "redirect:selectlist.do";
         }
-        Board board = qnaService.selectoneBoard(no); // no에 맞는 글 조회
-        log.info("QnAUpdate of selectone = {}", board); //수정화면으로 넘어와서의 board값
+        // Board board = qnaService.selectoneBoard(no); // no에 맞는 글 조회
+        // log.info("QnAUpdate of selectone = {}", board); //수정화면으로 넘어와서의 board값
 
-        model.addAttribute("board", board); // "board"에 조회된 board를 담아서 update.html로 보냄
+        // model.addAttribute("board", board); // "board"에 조회된 board를 담아서 update.html로 보냄
         return "/msh/update";
     }
     @PostMapping(value = "/update.do")
