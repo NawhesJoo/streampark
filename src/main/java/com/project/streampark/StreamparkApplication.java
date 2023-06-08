a875a0bf52def02bd7b3256df7efec3a683c9871
J3ONG <kenohjw@gmail.com>
2023-06-08 14:33:53 +0900

    1
    1

--- src/main/java/com/project/streampark/StreamparkApplication.java
+++ src/main/java/com/project/streampark/StreamparkApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
@@ -25,10 +27,15 @@ import org.springframework.data.web.config.PageableHandlerMethodArgumentResolver
 }) // 컨트롤러, 서비스 위치, 시큐리티 환경설정
 @EntityScan(basePackages = { "com.project.entity" }) // 엔티티 위치
 @EnableJpaRepositories(basePackages = { "com.project.repository" }) // 저장소 위치
-public class StreamparkApplication {
+public class StreamparkApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamparkApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StreamparkApplication.class);
 	}
 
 	@Bean
